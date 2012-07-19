4be0e27c5949ec15c2aec863bc4d86dbf0d22d9e
ywan171 <yi.a.wang@intel.com>
2012-07-19 19:07:31 +0800

    libstagefright: enable AVI playback on JB
    Change-Id: Ic5be2bd576e6d2e8494ab1816f195443e0f515b1
    Author: Yi Wang <yi.a.wang@intel.com>
    Signed-off-by: Yi Wang <yi.a.wang@intel.com>
    Signed-off-by: Xiaolin Zhang <xiaolin.zhang@intel.com>
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 47452

--- media/libstagefright/DataSource.cpp
+++ media/libstagefright/DataSource.cpp
@@ -19,6 +19,7 @@
 #if CHROMIUM_AVAILABLE
 #include "include/chromium_http_stub.h"
 #endif
+#include "include/AVIExtractor.h"
 
 #include "include/AACExtractor.h"
 #include "include/DRMExtractor.h"
@@ -122,6 +123,7 @@ void DataSource::RegisterDefaultSniffers() {
     RegisterSniffer(SniffAAC);
     RegisterSniffer(SniffMPEG2PS);
     RegisterSniffer(SniffWVM);
+    RegisterSniffer(SniffAVI);
 
     char value[PROPERTY_VALUE_MAX];
     if (property_get("drm.service.enabled", value, NULL)
