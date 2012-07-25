97b8303bd531e4cc0ffa7739b9c6b30b5ed67b44
Weian Chen <weian.chen@intel.com>
2012-07-25 11:20:37 -0400

    Add 100us scheduling interval between consecutive video events
    Add 100us scheduling interval between consecutive video events so that
    any attempts to cancel future video events has opportunity to be invoked
    during the 100us time slot. For BZ7871, after adding this patch, the
    suspend() methods could be invoked successfully after power button
    is pressed.
    
    Change-Id: I5f9fce75cc4a972c0c2dc4d739d946de4fff03d6
    Author: Weian Chen <weian.chen@intel.com>
    Signed-off-by: Dan Liang <dan.liang@intel.com>
    Signed-off-by: Weian Chen <weian.chen@intel.com>
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 48604, 7871, 18523

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1764,7 +1764,10 @@ void AwesomePlayer::onVideoEvent() {
                 mSeeking = SEEK_VIDEO_ONLY;
                 mSeekTimeUs = mediaTimeUs;
 
-                postVideoEvent_l();
+                // the next video event scheduling will occur after 100us so that
+                // any attempts to cancel future video events could take effect within
+                // this 100us interval
+                postVideoEvent_l(100);
                 return;
             } else {
                 // The widevine extractor doesn't deal well with seeking
@@ -1795,7 +1798,7 @@ void AwesomePlayer::onVideoEvent() {
                     ++mStats.mNumVideoFramesDropped;
                 }
 
-                postVideoEvent_l();
+                postVideoEvent_l(100);
                 return;
             }
         }
