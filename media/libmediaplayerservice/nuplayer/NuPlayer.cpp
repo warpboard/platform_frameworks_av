7d467393c59955a2a540a92152e07765dc77685c
Yi Wang <yi.a.wang@intel.com>
2012-07-29 07:48:23 +0800

    libmediaplayerservice:delay codec EOS or error message during NuPlayer flushing decoder.
    During NuPlayer flush decoder, codec reached EOS or error, and message it to NuPlayer.
    If NuPlayerRender has already flushed its video and audio queue, and NuPlayer
    still push EOS entry to NuPlayerRender queue, then crash happend because
    queue is not empty when finish flush checking queue.
    
    Change-Id: I9079ce282f91d149b4b773b533cf248b833e6e14
    Author: Yi Wang <yi.a.wang@intel.com>
    Signed-off-by: Yi Wang <yi.a.wang@intel.com>
    Signed-off-by: Dan Liang <dan.liang@intel.com>
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 48710 43878

--- media/libmediaplayerservice/nuplayer/NuPlayer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayer.cpp
@@ -57,6 +57,8 @@ NuPlayer::NuPlayer()
       mVideoIsAVC(false),
       mAudioEOS(false),
       mVideoEOS(false),
+      mAudioEosPending(false),
+      mVideoEosPending(false),
       mScanSourcesPending(false),
       mScanSourcesGeneration(0),
       mTimeDiscontinuityPending(false),
@@ -337,7 +339,16 @@ void NuPlayer::onMessageReceived(const sp<AMessage> &msg) {
                          err);
                 }
 
-                mRenderer->queueEOS(audio, err);
+                // we should delay EOS message during flushing
+                if (!IsFlushingState(audio ? mFlushingAudio : mFlushingVideo)) {
+                    mRenderer->queueEOS(audio, err);
+                } else if (audio) {
+                    mAudioEosPending = true;
+                    mAudioEosErr = err;
+                } else {
+                    mVideoEosPending = true;
+                    mVideoEosErr = err;
+                }
             } else if (what == ACodec::kWhatFlushCompleted) {
                 bool needShutdown;
 
@@ -455,7 +466,16 @@ void NuPlayer::onMessageReceived(const sp<AMessage> &msg) {
                 ALOGE("Received error from %s decoder, aborting playback.",
                      audio ? "audio" : "video");
 
-                mRenderer->queueEOS(audio, UNKNOWN_ERROR);
+                // we should delay EOS message during flushing
+                if (!IsFlushingState(audio ? mFlushingAudio : mFlushingVideo)) {
+                    mRenderer->queueEOS(audio, UNKNOWN_ERROR);
+                } else if (audio) {
+                    mAudioEosPending = true;
+                    mAudioEosErr = UNKNOWN_ERROR;
+                } else {
+                    mVideoEosPending = true;
+                    mVideoEosErr = UNKNOWN_ERROR;
+                }
             } else if (what == ACodec::kWhatDrainThisBuffer) {
                 renderBuffer(audio, codecRequest);
             } else {
@@ -631,6 +651,17 @@ void NuPlayer::finishFlushIfPossible() {
         mRenderer->signalTimeDiscontinuity();
         mTimeDiscontinuityPending = false;
     }
+
+    if (mAudioEosPending && mRenderer != NULL) {
+        mRenderer->queueEOS(true,  mAudioEosErr);
+    }
+
+    if (mVideoEosPending && mRenderer != NULL) {
+        mRenderer->queueEOS(false, mVideoEosErr);
+    }
+
+    mAudioEosPending = false;
+    mVideoEosPending = false;
 
     if (mAudioDecoder != NULL) {
         mAudioDecoder->signalResume();
