b4708d7c723239aa47ac995f9eaac8b863e1973f
Roger Jönsson <roger1.jonsson@sonymobile.com>
2013-01-24 11:50:08 +0100

    Skip non I-frames at startup and after seek for RTSP
    When seeking or starting an RTSP stream the video might be blurry
    until the first I-frame is rendered.
    This fix makes sure that the first rendered frame at startup and
    after seek is an I-frame (for AVC video).
    If an access unit containing SPS information arrives, the decoding
    is also started.
    
    Change-Id: I236bd97542f313a3ddcb27c8adf86d25f3f570d8

--- media/libmediaplayerservice/nuplayer/NuPlayer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayer.cpp
@@ -122,6 +122,7 @@ NuPlayer::NuPlayer()
       mTimeDiscontinuityPending(false),
       mFlushingAudio(NONE),
       mFlushingVideo(NONE),
+      mVideoSkipToIFrame(false),
       mSkipRenderingAudioUntilMediaTimeUs(-1ll),
       mSkipRenderingVideoUntilMediaTimeUs(-1ll),
       mVideoLateByUs(0ll),
@@ -822,6 +823,7 @@ status_t NuPlayer::instantiateDecoder(bool audio, sp<Decoder> *decoder) {
         AString mime;
         CHECK(format->findString("mime", &mime));
         mVideoIsAVC = !strcasecmp(MEDIA_MIMETYPE_VIDEO_AVC, mime.c_str());
+        mVideoSkipToIFrame = mVideoIsAVC;
     }
 
     sp<AMessage> notify =
@@ -944,6 +946,13 @@ status_t NuPlayer::feedDecoderInputData(bool audio, const sp<AMessage> &msg) {
             dropAccessUnit = true;
             ++mNumFramesDropped;
         }
+        if (!audio
+                && mVideoSkipToIFrame
+                && !IsIDR(accessUnit)
+                && !IsSPS(accessUnit)) {
+            ALOGV("Dropping non I-frame or SPS access unit after seek");
+            dropAccessUnit = true;
+        }
     } while (dropAccessUnit);
 
     // ALOGV("returned a valid buffer of %s data", audio ? "audio" : "video");
@@ -956,6 +965,9 @@ status_t NuPlayer::feedDecoderInputData(bool audio, const sp<AMessage> &msg) {
          mediaTimeUs / 1E6);
 #endif
 
+    if (!audio && mVideoSkipToIFrame) {
+        mVideoSkipToIFrame = false;
+    }
     reply->setBuffer("buffer", accessUnit);
     reply->post();
 
@@ -1054,6 +1066,7 @@ void NuPlayer::flushDecoder(bool audio, bool needShutdown) {
                 || mFlushingVideo == AWAITING_DISCONTINUITY);
 
         mFlushingVideo = newStatus;
+        mVideoSkipToIFrame = mVideoIsAVC;
 
         if (mFlushingAudio == NONE) {
             mFlushingAudio = (mAudioDecoder != NULL)
