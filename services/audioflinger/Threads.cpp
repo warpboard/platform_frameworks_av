d6aa6c01be121042041aba34a6d4de726f5d788c
Shengjiu Wang <b02247@freescale.com>
2013-08-19 14:36:13 +0800

    ENGR00265111 Video freezed for a short while when audio direct output
    Add CBLK_DISABLED_ON flag for direct output, when underrun happened,
    AudioTrack can restart the DirectOutput thread.
    
    cherry-picked from jb4.2.2 610cd31f22839ce88bb0aff1bce5993b98b7af55
    
    Signed-off-by: Shengjiu Wang <b02247@freescale.com>

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -1,6 +1,7 @@
 /*
 **
 ** Copyright 2012, The Android Open Source Project
+** Copyright (C) 2013 Freescale Semiconductor, Inc.
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -3241,6 +3242,7 @@ AudioFlinger::PlaybackThread::mixer_state AudioFlinger::DirectOutputThread::prep
                 if (--(track->mRetryCount) <= 0) {
                     ALOGV("BUFFER TIMEOUT: remove(%d) from active list", track->name());
                     tracksToRemove->add(track);
+                    android_atomic_or(CBLK_DISABLED, &cblk->flags);
                 } else if (i == (count -1)){
                     mixerStatus = MIXER_TRACKS_ENABLED;
                 }
