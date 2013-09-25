28e6ae99e28854832e2cec466059b0dbbe67e4f8
guoyin.chen <guoyin.chen@freescale.com>
2013-09-25 23:19:51 +0800

    ENGR00281301 Play the streaming with stagefrighter for widevine content
    Using widevine API to check the streaming type
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- media/libmediaplayerservice/MediaPlayerFactory.cpp
+++ media/libmediaplayerservice/MediaPlayerFactory.cpp
@@ -15,7 +15,7 @@
 ** limitations under the License.
 */
 
-/* Copyright 2013 Freescale Semiconductor Inc. */
+/* Copyright (C) 2013 Freescale Semiconductor, Inc. */
 
 #define LOG_TAG "MediaPlayerFactory"
 #include <utils/Log.h>
@@ -39,6 +39,7 @@
 #include "../libstagefright/include/NuCachedSource2.h"
 #include "../libstagefright/include/WVMExtractor.h"
 #include <media/stagefright/DataSource.h>
+#include <media/stagefright/FileSource.h>
 #include <media/OMXPlayer.h>
 #endif
 
@@ -279,6 +280,40 @@ bool isWVM(const char* url,
     return false;
 }
 
+bool isWVM(int fd,
+           int64_t offset,
+           int64_t length) {
+    sp<DataSource> dataSource;
+
+    void *VendorLibHandle = NULL;
+    if (VendorLibHandle == NULL) {
+        VendorLibHandle = dlopen("libwvm.so", RTLD_NOW);
+    }
+
+    if (!VendorLibHandle) {
+        return false;
+    }
+
+    dataSource = new FileSource(dup(fd), offset, length);
+
+    if (dataSource == NULL) {
+        return false;
+    }
+
+    typedef WVMLoadableExtractor *(*SnifferFunc)(const sp<DataSource>&);
+    SnifferFunc snifferFunc =
+        (SnifferFunc) dlsym(VendorLibHandle,
+                "_ZN7android15IsWidevineMediaERKNS_2spINS_10DataSourceEEE");
+
+    if (snifferFunc) {
+        if ((*snifferFunc)(dataSource)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 class OMXPlayerFactory : public MediaPlayerFactory::IFactory {
     public:
         virtual float scoreFactory(const sp<IMediaPlayer>& client,
@@ -372,6 +407,9 @@ class OMXPlayerFactory : public MediaPlayerFactory::IFactory {
             }
 
             if (kOurScore <= curScore)
+                return 0.0;
+
+            if (isWVM(fd, offset, length))
                 return 0.0;
 
             char url[128];
